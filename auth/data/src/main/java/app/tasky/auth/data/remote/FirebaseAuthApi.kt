63b92e8bc254517626880cbadb5d49d77c5f5363
Usmon Abdurakhmanov <usdaves@gmail.com>
2023-03-14 11:33:53 +0500

    `FirebaseAuthApi` class created (#15)

---
+++ auth/data/src/main/java/app/tasky/auth/data/remote/FirebaseAuthApi.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2023 usdaves(Usmon Abdurakhmanov)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package app.tasky.auth.data.remote
+
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.tasks.await
+
+// Created by usdaves(Usmon Abdurakhmanov) on 3/14/2023
+
+internal class FirebaseAuthApi(
+  private val firebaseAuth: FirebaseAuth,
+) : AuthApi {
+
+  override suspend fun signIn(email: String, password: String): String {
+    val authResult = firebaseAuth.signInWithEmailAndPassword(email, password).await()
+    return authResult.user?.uid
+      ?: TODO("Haven't been implemented yet. We have to let the user know why the uid is null")
+  }
+
+  override suspend fun signUp(email: String, password: String): String {
+    val authResult = firebaseAuth.createUserWithEmailAndPassword(email, password).await()
+    return authResult.user?.uid
+      ?: TODO("Haven't been implemented yet. We have to let the user know why the uid is null")
+  }
+}
